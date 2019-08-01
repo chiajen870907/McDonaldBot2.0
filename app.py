d08b24ebceeed3c9e79d155dadd79ac736ed0654
HsiehChiaJen <bboy77520@gmail.com>
2019-08-02 02:39:07 +0800

    Add Code

--- app.py
+++ app.py
@@ -173,7 +173,8 @@ def handle_message(event):
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text='請重新輸入-多打了斜線了'))
         line_bot_api.push_message(user_id, TextSendMessage(text='帳號:{}\n密碼:{}\n正在嘗試登入OwO'.format(t[0], t[1])))
         global MC_User_ID, MC_User_PASSWORD
-        MC_User_ID = t[0], MC_User_PASSWORD = t[1]
+        MC_User_ID = t[0]
+        MC_User_PASSWORD = t[1]
         login_MC()
 
 
