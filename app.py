27148ca59d6d99ffbef33847e5e414aff90b4299
HsiehChiaJen <bboy77520@gmail.com>
2019-08-01 22:25:25 +0800

    Add Code

--- app.py
+++ app.py
@@ -40,3 +40,39 @@ import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+
+
+
+#-----
+# 引用必要套件
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# 引用私密金鑰
+# path/to/serviceAccount.json 請用自己存放的路徑
+cred = credentials.Certificate('/service-account.json')
+
+# 初始化firebase，注意不能重複初始化
+firebase_admin.initialize_app(cred)
+
+# 初始化firestore
+db = firestore.client()
+
+doc = {
+  'name': "帽子哥",
+  'email': "abc@gmail.com"
+}
+
+# 建立文件 必須給定 集合名稱 文件id
+# 即使 集合一開始不存在 都可以直接使用
+
+# 語法
+# doc_ref = db.collection("集合名稱").document("文件id")
+
+doc_ref = db.collection("pyradise_students").document("student_01")
+
+# doc_ref提供一個set的方法，input必須是dictionary
+
+doc_ref.set(doc)
