859da6501a1364e43d10359b236582b15161e77d
HsiehChiaJen <bboy77520@gmail.com>
2019-08-01 22:56:58 +0800

    Add Code

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ line_bot_api = LineBotApi('a4ZRk4l00GSRM9haYsEAdV90WTEk+LMkWCI71MqObTkXFq8ygRUlb
 # Channel Secret
 handler = WebhookHandler('62aab4cbbb8fe1efcfd845bc9211e748')
 # 引用私密金鑰
-#cred = credentials.Certificate('/service-account.json')
+cred = credentials.Certificate('/app/service-account.json')
 #default_app = firebase_admin.initialize_app()
 print(os.getcwd())
 # 監聽所有來自 /callback 的 Post Request
