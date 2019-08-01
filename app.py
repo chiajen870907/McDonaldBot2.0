c742e45def536b9c283810b7c7d77b61825ac2bf
HsiehChiaJen <bboy77520@gmail.com>
2019-08-02 00:39:08 +0800

    Add Code

--- app.py
+++ app.py
@@ -46,8 +46,8 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    # message = TextSendMessage(text=event.message.text)
+    # line_bot_api.reply_message(event.reply_token, message)
     user_id = event.source.user_id
 
     if event.message.text == "Login":
