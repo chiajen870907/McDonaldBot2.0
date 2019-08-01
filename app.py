015d3129d9ee6140485bebafc3eb155382497f20
HsiehChiaJen <bboy77520@gmail.com>
2019-08-01 22:30:13 +0800

    Add Code

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
 app = Flask(__name__)
 
@@ -14,6 +12,12 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('a4ZRk4l00GSRM9haYsEAdV90WTEk+LMkWCI71MqObTkXFq8ygRUlbwD7qxeS0+vNX+bMN0FvnTP91dASCXNBuxw5HdN0/vCKcSQxIw+QE4u09ARZUmxg9Cg7NMBfn2EBCpfxNXN70UIDg+YwAs130wdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
 handler = WebhookHandler('62aab4cbbb8fe1efcfd845bc9211e748')
+# 引用私密金鑰
+cred = credentials.Certificate('/service-account.json')
+# 初始化firebase，注意不能重複初始化
+firebase_admin.initialize_app(cred)
+# 初始化firestore
+db = firestore.client()
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -35,7 +39,15 @@ def callback():
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
-
+    user_id = event.source.user_id
+    doc = {
+        'name': user_id,
+        'email': "abc@gmail.com"
+    }
+    # doc_ref = db.collection("集合名稱").document("文件id")
+    doc_ref = db.collection("pyradise_students").document("student_01")
+    # doc_ref提供一個set的方法，input必須是dictionary
+    doc_ref.set(doc)
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
@@ -44,35 +56,11 @@ if __name__ == "__main__":
 
 
 
-#-----
-# 引用必要套件
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
 
-# 引用私密金鑰
-# path/to/serviceAccount.json 請用自己存放的路徑
-cred = credentials.Certificate('/service-account.json')
 
-# 初始化firebase，注意不能重複初始化
-firebase_admin.initialize_app(cred)
 
-# 初始化firestore
-db = firestore.client()
 
-doc = {
-  'name': "帽子哥",
-  'email': "abc@gmail.com"
-}
 
-# 建立文件 必須給定 集合名稱 文件id
-# 即使 集合一開始不存在 都可以直接使用
 
-# 語法
-# doc_ref = db.collection("集合名稱").document("文件id")
 
-doc_ref = db.collection("pyradise_students").document("student_01")
 
-# doc_ref提供一個set的方法，input必須是dictionary
-
-doc_ref.set(doc)
