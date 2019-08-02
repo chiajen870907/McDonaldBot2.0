de42b511af5471733065b83e0127e13014be9b58
HsiehChiaJen <bboy77520@gmail.com>
2019-08-02 14:00:47 +0800

    Add Code

--- app.py
+++ app.py
@@ -219,14 +219,13 @@ def handle_message(event):
     user_id = event.source.user_id
     # ----------------Login-----------------------
     Count_Index = int(Database_Counter_GetCount())
-    for i in range(0, Count_Index):
-        print(i)
-        # path = ("Line_User/User" + str(Count_Index))
-        # print(path)
-        # doc_ref = db.document(path)
-        # doc = doc_ref.get()
-        # result = doc.to_dict()
-        # print(result)
+    for i in range(1, Count_Index):
+        path = ("Line_User/User" + str(i))
+        #print(path)
+        doc_ref = db.document(path)
+        doc = doc_ref.get()
+        result = doc.to_dict()
+        print(result)
         # # if result == None:
 
 if __name__ == "__main__":
