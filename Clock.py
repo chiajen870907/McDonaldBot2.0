2ea695fc33bafcca995f9a95e671cda5c185f0d5
HsiehChiaJen <bboy77520@gmail.com>
2019-08-03 20:54:59 +0800

    New Line Chanle

--- Clock.py
+++ Clock.py
@@ -1,9 +1,11 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from app import McDonald_Lottery
 
+
+
 sched = BlockingScheduler()
-@sched.Lottery('cron', hour=0)
-def Lottery():
+@sched.scheduled_job('cron', hour=0)
+def scheduled_job():
     McDonald_Lottery()
 sched.start()
 
