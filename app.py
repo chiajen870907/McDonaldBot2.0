3a65ba58424400b76ab1d59fb185215c3be6f8e9
HsiehChiaJen <bboy77520@gmail.com>
2019-08-03 16:16:45 +0800

    Add Code

--- app.py
+++ app.py
@@ -236,15 +236,15 @@ def Database_Check_UserID():
         temp_ID = str(doc.to_dict())
         # print('temp_ID ', temp_ID)
         result_ID = re.search(user_id, temp_ID)
-        print('result_ID', result_ID)
-        # if result_ID is None:
-        #     UserID_Exists = 0
-        #     print('CantFind')
-        # else:
-        #     UserID_Exists = 1
-        #     re.search(user_id, result)
-        #     print('Find_UserID')
-        #     break
+        #print('result_ID', result_ID)
+        if result_ID is None:
+            UserID_Exists = 0
+            print('CantFind')
+        else:
+            UserID_Exists = 1
+            #re.search(user_id, result)
+            print('Find_UserID')
+            break
 
 
 
