514dc934bf66d73339a9a87b4d96c3f1361cacf3
HsiehChiaJen <bboy77520@gmail.com>
2019-08-03 20:36:09 +0800

    Add Code

--- test.py
+++ test.py
@@ -1,14 +1,9 @@
-# import re
-# PushID = " { 'UserID' : 'UEAXSSID USERID1 UserID11'} "
-# PushID = re.sub("[{} \' :]", "", str(PushID))
-# PushID = PushID.replace('UserID','')
-# print(PushID)
-def McDonald_Lottery():
-    print('f')
-
-from apscheduler.schedulers.blocking import BlockingScheduler
-scheduler = BlockingScheduler()
-# 添加任务并设置触发方式为3s一次
-scheduler.add_job(McDonald_Lottery, 'interval', minutes=1)
-# 开始运行调度器
-scheduler.start()
+# # import re
+# # PushID = " { 'UserID' : 'UEAXSSID USERID1 UserID11'} "
+# # PushID = re.sub("[{} \' :]", "", str(PushID))
+# # PushID = PushID.replace('UserID','')
+# # print(PushID)
+# def McDonald_Lottery():
+#     print('f')
+#
+# from apscheduler.schedulers.blocking import BlockingScheduler
