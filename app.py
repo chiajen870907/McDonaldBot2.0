ef58e1d4485c86c2790f21cf9ec75441ccc030c4
HsiehCJ <chiajen870907@gmail.com>
2019-10-26 00:03:42 +0800

    1

--- app.py
+++ app.py
@@ -349,82 +349,119 @@ def handle_message(event):
                 line_bot_api.reply_message(event.reply_token, TextSendMessage(text='o_O ||\n你沒有任何優惠卷ㅇㅁㅇ'))
             else:
                 line_bot_api.reply_message(event.reply_token, TextSendMessage(text=Coupon_List_result))
+
         elif event.message.text == '貼紙抽獎':
             Coupon_List_result = McDonald_Get_CouponList()
             if Coupon_List_result == '':
                 line_bot_api.reply_message(event.reply_token, TextSendMessage(text='o_O ||\n你沒有任何優惠卷ㅇㅁㅇ'))
             else:
-
                 line_bot_api.reply_message(event.reply_token, TextSendMessage(text=Coupon_List_result))
 
-        elif event.message.text == 't':
-            bubble_string = """"
-    {
-  "type": "carousel",
-  "contents": [
-    {
-      "type": "bubble",
-      "body": {
-        "type": "box",
-        "layout": "vertical",
-        "contents": [
-          {
-            "type": "image",
-            "url": "https://mcdapp1.azureedge.net/P_S148.jpg",
-            "size": "full",
-            "aspectMode": "cover",
-            "aspectRatio": "2:3",
-            "gravity": "top"
-          },
-          {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-              {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "截止日期",
-                    "size": "xl",
-                    "color": "#000000",
-                    "weight": "bold",
-                    "flex": 0
-                  }
-                ]
-              },
-              {
-                "type": "box",
-                "layout": "baseline",
-                "contents": [
-                  {
-                    "type": "text",
-                    "text": "2019/10/25 23:59:59",
-                    "color": "#FF0000",
-                    "flex": 0,
-                    "size": "lg"
-                  }
-                ],
-                "spacing": "lg"
-              }
-            ],
-            "position": "absolute",
-            "offsetBottom": "0px",
-            "offsetStart": "0px",
-            "offsetEnd": "0px",
-            "backgroundColor": "#F5FFFA",
-            "paddingAll": "20px",
-            "paddingTop": "18px"
-          }
-        ],
-        "paddingAll": "0px"
-      }
-    }
-  ]
-}"""
-            message = FlexSendMessage(alt_text="hello", contents=json.loads(bubble_string))
-            line_bot_api.reply_message(event.reply_token, message)
+        elif event.message.text == 'flex':
+            bubble = BubbleContainer(
+                direction='ltr',
+                hero=ImageComponent(
+                    url='https://example.com/cafe.jpg',
+                    size='full',
+                    aspect_ratio='20:13',
+                    aspect_mode='cover',
+                    action=URIAction(uri='http://example.com', label='label')
+                ),
+                body=BoxComponent(
+                    layout='vertical',
+                    contents=[
+                        # title
+                        TextComponent(text='Brown Cafe', weight='bold', size='xl'),
+                        # review
+                        BoxComponent(
+                            layout='baseline',
+                            margin='md',
+                            contents=[
+                                IconComponent(size='sm', url='https://example.com/gold_star.png'),
+                                IconComponent(size='sm', url='https://example.com/grey_star.png'),
+                                IconComponent(size='sm', url='https://example.com/gold_star.png'),
+                                IconComponent(size='sm', url='https://example.com/gold_star.png'),
+                                IconComponent(size='sm', url='https://example.com/grey_star.png'),
+                                TextComponent(text='4.0', size='sm', color='#999999', margin='md',
+                                              flex=0)
+                            ]
+                        ),
+                        # info
+                        BoxComponent(
+                            layout='vertical',
+                            margin='lg',
+                            spacing='sm',
+                            contents=[
+                                BoxComponent(
+                                    layout='baseline',
+                                    spacing='sm',
+                                    contents=[
+                                        TextComponent(
+                                            text='Place',
+                                            color='#aaaaaa',
+                                            size='sm',
+                                            flex=1
+                                        ),
+                                        TextComponent(
+                                            text='Shinjuku, Tokyo',
+                                            wrap=True,
+                                            color='#666666',
+                                            size='sm',
+                                            flex=5
+                                        )
+                                    ],
+                                ),
+                                BoxComponent(
+                                    layout='baseline',
+                                    spacing='sm',
+                                    contents=[
+                                        TextComponent(
+                                            text='Time',
+                                            color='#aaaaaa',
+                                            size='sm',
+                                            flex=1
+                                        ),
+                                        TextComponent(
+                                            text="10:00 - 23:00",
+                                            wrap=True,
+                                            color='#666666',
+                                            size='sm',
+                                            flex=5,
+                                        ),
+                                    ],
+                                ),
+                            ],
+                        )
+                    ],
+                ),
+                footer=BoxComponent(
+                    layout='vertical',
+                    spacing='sm',
+                    contents=[
+                        # callAction, separator, websiteAction
+                        SpacerComponent(size='sm'),
+                        # callAction
+                        ButtonComponent(
+                            style='link',
+                            height='sm',
+                            action=URIAction(label='CALL', uri='tel:000000'),
+                        ),
+                        # separator
+                        SeparatorComponent(),
+                        # websiteAction
+                        ButtonComponent(
+                            style='link',
+                            height='sm',
+                            action=URIAction(label='WEBSITE', uri="https://example.com")
+                        )
+                    ]
+                ),
+            )
+            message = FlexSendMessage(alt_text="hello", contents=bubble)
+            line_bot_api.reply_message(
+                event.reply_token,
+                message
+            )
 
         else:
 
