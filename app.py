2dd7765632398a3863bf11a1dbd580a76a188135
HsiehCJ <chiajen870907@gmail.com>
2019-10-25 22:38:17 +0800

    s

--- app.py
+++ app.py
@@ -357,99 +357,142 @@ def handle_message(event):
 
                 line_bot_api.reply_message(event.reply_token, TextSendMessage(text=Coupon_List_result))
 
-        elif event.message.text == "Carousel template":
+        elif event.message.text == "test":
             message = TemplateSendMessage(text={
-  "type": "carousel",
-  "contents": [
-	{
-	  "type": "bubble",
-	  "hero": {
-		"type": "image",
-		"url": "https://mcdapp1.azureedge.net/P_S148.jpg",
-		"size": "full",
-		"aspectRatio": "1:1",
-		"aspectMode": "cover",
-		"action": {
-		  "type": "uri",
-		  "uri": "http://linecorp.com/"
-		}
-	  },
-	  "body": {
-		"type": "box",
-		"layout": "vertical",
-		"contents": [
-		  {
-			"type": "text",
-			"text": "2019/10/25",
-			"weight": "bold",
-			"size": "xl",
-			"color": "#00BD00"
-		  },
-		  {
-			"type": "box",
-			"layout": "vertical",
-			"margin": "lg",
-			"spacing": "sm",
-			"contents": [
-			  {
-				"type": "box",
-				"layout": "baseline",
-				"spacing": "sm",
-				"contents": [
-				  {
-					"type": "text",
-					"text": "截止日期  ",
-					"color": "#aaaaaa",
-					"size": "sm",
-					"flex": 1
-				  },
-				  {
-					"type": "text",
-					"text": "2019/10/25 23:59:59",
-					"wrap": 'true',
-					"size": "sm",
-					"weight": "regular",
-					"style": "normal",
-					"decoration": "none",
-					"position": "relative",
-					"flex": 2,
-					"color": "#FF1212"
-				  }
-				]
-			  },
-			  {
-				"type": "box",
-				"layout": "baseline",
-				"spacing": "sm",
-				"contents": [
-				  {
-					"type": "text",
-					"text": "CouponID",
-					"color": "#aaaaaa",
-					"size": "sm",
-					"flex": 1,
-					"align": "start"
-				  },
-				  {
-					"type": "text",
-					"text": "1074495258",
-					"align": "start",
-					"flex": 2,
-					"size": "sm"
-				  }
-				]
-			  }
-			]
-		  }
-		]
-	  },
-	  "styles": {
-		"footer": {
-		  "separator": 'true'
-		}
-	  }
-	}
-  ]
+  "type": "bubble",
+  "header": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "FIFA World Cup 2018",
+        "size": "xl",
+        "weight": "bold"
+      }
+    ]
+  },
+  "hero": {
+    "type": "image",
+    "url": "https://sitthi.me:3807/static/fifa.jpg",
+    "size": "full",
+    "aspectRatio": "20:13",
+    "aspectMode": "cover"
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "spacing": "md",
+    "contents": [
+      {
+        "type": "box",
+        "layout": "horizontal",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "text",
+            "text": "LIVE !",
+            "size": "lg",
+            "color": "#555555",
+            "weight": "bold",
+            "align": "center"
+          }
+        ]
+      },
+      {
+        "type": "button",
+        "style": "primary",
+        "action": {
+          "type": "postback",
+          "label": "Portugal  1 : 0  Morocco",
+          "displayText": "Live Report !!",
+          "data": "LIVE"
+        }
+      },
+      {
+        "type": "separator",
+        "margin": "lg"
+      },
+      {
+        "type": "box",
+        "layout": "vertical",
+        "margin": "lg",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "button",
+                "style": "primary",
+                "action": {
+                  "type": "postback",
+                  "label": "Last Match",
+                  "displayText": "Last Match",
+                  "data": "LAST"
+                }
+              },
+              {
+                "type": "button",
+                "style": "primary",
+                "action": {
+                  "type": "postback",
+                  "label": "Next Match",
+                  "displayText": "Next Match",
+                  "data": "NEXT"
+                }
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "button",
+                "style": "primary",
+                "action": {
+                  "type": "postback",
+                  "label": "Schedule",
+                  "displayText": "Schedule",
+                  "data": "SCHEDULE"
+                }
+              },
+              {
+                "type": "button",
+                "style": "primary",
+                "action": {
+                  "type": "postback",
+                  "label": "Table",
+                  "displayText": "Table",
+                  "data": "TABLE"
+                }
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "button",
+        "margin": "sm",
+        "action": {
+          "type": "uri",
+          "label": "View Source",
+          "uri": "https://sitthi.me:3807/downloaded/ba5f784d837540dfb40df2d531d7519c.json"
+        },
+        "style": "secondary"
+      }
+    ]
+  }
 })
             line_bot_api.reply_message(event.reply_token, message)
 
