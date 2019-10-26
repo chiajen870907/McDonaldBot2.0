5c666aa25133a4e7494b4ac051362ae221e25763
HsiehCJ <chiajen870907@gmail.com>
2019-10-26 12:36:13 +0800

    ImageCarousel template

--- app.py
+++ app.py
@@ -52,8 +52,6 @@ class Mask(object):
         self.OrderNo = self.DeviceUuid + self.str3                         # Order No
         self.cardNo = 'cardNo'                                             # Card NO
 
-
-
     def Login(self):
         # Mask = MD5('Mc' + OrderNo + Platform + OsVersion + ModelId + DeviceUuid + str1 + str2 + paramString + 'Donalds')
         data = 'Mc%s%s%s%s%s%s%s%sDonalds' % (
@@ -335,10 +333,11 @@ def handle_message(event):
     user_id = event.source.user_id
     # ----------------Login-----------------------
     if db.collection('Check').document(user_id).get().exists == True:
-        print('存在')
+        # print('存在')
         if event.message.text == '歡樂貼':
             result = McDonald_Get_StickerList()
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text='目前擁有歡樂貼:{}\n月底即將到期歡樂貼:{}'.format(result[0], result[1])))
+
         elif event.message.text == '抽獎':
             Coupon_result = Request_Coupon_Lottery()
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=Coupon_result))
@@ -358,110 +357,32 @@ def handle_message(event):
                 line_bot_api.reply_message(event.reply_token, TextSendMessage(text=Coupon_List_result))
 
         elif event.message.text == 'flex':
-            bubble = BubbleContainer(
-                direction='ltr',
-                hero=ImageComponent(
-                    url='https://example.com/cafe.jpg',
-                    size='full',
-                    aspect_ratio='20:13',
-                    aspect_mode='cover',
-                    action=URIAction(uri='http://example.com', label='label')
-                ),
-                body=BoxComponent(
-                    layout='vertical',
-                    contents=[
-                        # title
-                        TextComponent(text='Brown Cafe', weight='bold', size='xl'),
-                        # review
-                        BoxComponent(
-                            layout='baseline',
-                            margin='md',
-                            contents=[
-                                IconComponent(size='sm', url='https://example.com/gold_star.png'),
-                                IconComponent(size='sm', url='https://example.com/grey_star.png'),
-                                IconComponent(size='sm', url='https://example.com/gold_star.png'),
-                                IconComponent(size='sm', url='https://example.com/gold_star.png'),
-                                IconComponent(size='sm', url='https://example.com/grey_star.png'),
-                                TextComponent(text='4.0', size='sm', color='#999999', margin='md',
-                                              flex=0)
-                            ]
+            message = TemplateSendMessage(
+                alt_text='ImageCarousel template',
+                template=ImageCarouselTemplate(
+                    columns=[
+                        ImageCarouselColumn(
+                            image_url='https://mcdapp1.azureedge.net/P_S148.jpg',
+                            action=PostbackTemplateAction(
+                                label='postback1',
+                                text='postback text1',
+                                data='action=buy&itemid=1'
+                            )
                         ),
-                        # info
-                        BoxComponent(
-                            layout='vertical',
-                            margin='lg',
-                            spacing='sm',
-                            contents=[
-                                BoxComponent(
-                                    layout='baseline',
-                                    spacing='sm',
-                                    contents=[
-                                        TextComponent(
-                                            text='Place',
-                                            color='#aaaaaa',
-                                            size='sm',
-                                            flex=1
-                                        ),
-                                        TextComponent(
-                                            text='Shinjuku, Tokyo',
-                                            wrap=True,
-                                            color='#666666',
-                                            size='sm',
-                                            flex=5
-                                        )
-                                    ],
-                                ),
-                                BoxComponent(
-                                    layout='baseline',
-                                    spacing='sm',
-                                    contents=[
-                                        TextComponent(
-                                            text='Time',
-                                            color='#aaaaaa',
-                                            size='sm',
-                                            flex=1
-                                        ),
-                                        TextComponent(
-                                            text="10:00 - 23:00",
-                                            wrap=True,
-                                            color='#666666',
-                                            size='sm',
-                                            flex=5,
-                                        ),
-                                    ],
-                                ),
-                            ],
-                        )
-                    ],
-                ),
-                footer=BoxComponent(
-                    layout='vertical',
-                    spacing='sm',
-                    contents=[
-                        # callAction, separator, websiteAction
-                        SpacerComponent(size='sm'),
-                        # callAction
-                        ButtonComponent(
-                            style='link',
-                            height='sm',
-                            action=URIAction(label='CALL', uri='tel:000000'),
-                        ),
-                        # separator
-                        SeparatorComponent(),
-                        # websiteAction
-                        ButtonComponent(
-                            style='link',
-                            height='sm',
-                            action=URIAction(label='WEBSITE', uri="https://example.com")
+                        ImageCarouselColumn(
+                            image_url='https://mcdapp1.azureedge.net/P_S148.jpg',
+                            action=PostbackTemplateAction(
+                                label='postback2',
+                                text='postback text2',
+                                data='action=buy&itemid=2'
+                            )
                         )
                     ]
-                ),
+                )
             )
-            message = FlexSendMessage(alt_text="hello", contents=bubble)
-            line_bot_api.reply_message(
-                event.reply_token,
-                message
-            )
+            line_bot_api.reply_message(event.reply_token, message)
+
+
 
         else:
 
