51f855543afa54b6d04b2c77cc93f9947ee74036
HsiehCJ <chiajen870907@gmail.com>
2019-11-08 18:12:50 +0800

    Signed-off-by: HsiehCJ <chiajen870907@gmail.com>

--- app.py
+++ app.py
@@ -333,7 +333,7 @@ def handle_message(event):
     global account
     user_id = event.source.user_id
     # ----------------Login-----------------------
-    if Database_Check_UserState()[0]:
+    if Database_Check_UserState(user_id)[0]:
         if event.message.text == '我的歡樂貼':
             StickerList = McDonald_Get_StickerList()
             line_bot_api.reply_message(event.reply_token, TextSendMessage(
